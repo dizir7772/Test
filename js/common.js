2c56d4d8d3eb49b82e1734dcfb9e978eebaecbc9
dizir7772 <v.a.artemenko1990@gmail.com>
2017-09-08 09:28:17 +0300

    dixie

---
+++ js/common.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.single-item').slick({
+        // arrows:true,
+        dots: true,
+        responsive: [
+            {   breakpoint: 1270,
+                settings: {
+                    arrows: false
+                }
+            }
+            ]
+    });
+});
+
+
+
+
